6aac1d7e06f19c867910df04caa0cbeeadd43cd5
Noah <85143444+npstrass@users.noreply.github.com>
2021-08-04 14:50:21 -0500

    Initial commit

---
+++ src/HOC/withToggler.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+class Toggler extends Component {
+  state = {
+    on: this.props.defaultOnValue
+  };
+
+  toggle = () => {
+    this.setState((prevState) => {
+      return {
+        on: !prevState.on
+      };
+    });
+  };
+
+  render() {
+    const Component = this.props.component;
+    return (
+      <Component on={this.state.on} toggle={this.toggle} {...this.props} />
+    );
+  }
+}
+
+export function withToggler(component, optionsObj) {
+  return function (props) {
+    return (
+      <Toggler
+        component={component}
+        defaultOnValue={optionsObj.defaultOnValue}
+        {...props}
+      />
+    );
+  };
+}
